f324dbdfbd391b7b302f6345c923616206cda315
imthaivu <imquocthai@gmail.com>
2024-12-06 11:16:36 +0700

    up b28 finish

--- BaiHoc/b28ReacJST2/hoc-bai-28/src/components/NavigationMenu/index.js
+++ BaiHoc/b28ReacJST2/hoc-bai-28/src/components/NavigationMenu/index.js
@@ -1,15 +1,38 @@
 function NavigationMenu() {
     const handleClick = (e) => {
-        console.log(e.target)
+
+        console.log("click", e.target)
     }
+    const handleChange = (e) => {
+        console.log(e.target.value)
+    }
+    const handleFocus = (e) => {
+        e.target.classList.add("inputActive")
+        console.log("focus", e.target)
+    }
+    const handleBlur = (e) => {
+        e.target.classList.remove("inputActive")
+        console.log("remove", e.target)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submit", e.target)
+        console.log("submit", e.target[0].value)
+    }
+
     return (
         <>
             <div className="box">
                 <div>navigation menu</div>
-                {/* luon goi ham */}
-                {/* <button onClick={handleClick()}>click me</button> */}
-                {/* khi co click thi moi goi ham */}
-                <button onClick={handleClick}>click me</button>
+                {/* react khong load lai trang */}
+                <form onSubmit={handleSubmit}>
+                    {/* luon goi ham */}
+                    {/* <button onClick={handleClick()}>click me</button> */}
+                    {/* khi co click thi moi goi ham */}
+                    <input type="text" onChange={handleChange} onFocus={handleFocus} onBlur={handleBlur} />
+                    {/* mac dinh la type submit */}
+                    <button onClick={handleClick}>search</button>
+                </form>
 
             </div>
         </>
